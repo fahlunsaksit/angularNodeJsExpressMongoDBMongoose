c6d828730da662e152ca4c7e54baa314f9a24244
fahlunsaksit <saksit.ban@gmail.com>
2019-09-21 22:26:57 +0700

    next MongoDB

--- node/server.js
+++ node/server.js
@@ -7,6 +7,15 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
+//Allow client to access cross domian or ip-addres
+app.use(function(req,res,next){
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers','content-type,x-access-token');
+    res.setHeader('Access-Control-Allow-Credentials','true');
+    next();
+});
+
 
 app.get('/',(req,res)=>{
     res.end("welcome to root path");
@@ -14,6 +23,14 @@ app.get('/',(req,res)=>{
 
 app.get('/home',(req,res)=>{
     res.end("welcome to home");
+});
+
+app.post('/api',(req,res)=>{
+    const feedback = req.body.feedback;
+    const username = req.body.username;
+
+    res.json({result: "success",username: username,feedback: feedback});
+    // res.end("feedback: "+feedback + " => User: "+user);
 });
 
 app.listen(3000,()=>{
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angularNodeJsExpressMongoDBMongoose';
+  
+  constructor(private http: HttpClient){};
+
+  ngOnInit(): void {
+
+  }
+
+  onSubmit(data){
+    alert(JSON.stringify(data));
+    // let postData = {username:data.email,feedback:data.feedback}
+      this.http.post<any>('http://localhost:3000/api',data).subscribe(result=>{
+        // alert(JSON.stringify(result));
+        alert(JSON.stringify(result));
+      });
+  }
 }
